6ad3b67b2904a280793cbd7d811ae419b0cc2c30
ChanGu3 <154095535+ChanGu3@users.noreply.github.com>
2025-11-15 22:37:14 -0800

    feat: db-setup + hard coded accounts + exposed login, logout, and user data

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,23 @@
+from db_setup import create_db
+create_db.setup_theia_db()
+
 from flask import Flask, jsonify
 from flask_cors import CORS
+from routes.api_routes import api_bp
 
 app = Flask(__name__)
 CORS(app)
+
+app.secret_key = 'fake_key_seriously_its_fake'
 
 @app.route('/')
 def home():
     return jsonify({"message": "Hello from Python!", "status": "running"})
 
-@app.route('/api/data')
-def get_data():
-    return jsonify({"data": ["Item 1", "Item 2", "Item 3"]})
+# routes - /api
+app.register_blueprint(api_bp)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
---
+++ backend/services/database.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+import sqlite3
+import json
+db_path = Path(__file__).parent.parent / "data" / "theia_db.db"
+
+class database :
+
+    #
+    # returns none when none otherwise a json of rows or row if singular
+    #
+    @staticmethod
+    def __create_json(data):
+        if (data is None):
+            return None
+        elif (isinstance(data, list)):
+            print("HERE")
+            return json.dumps( (dict(data_item) for data_item in data))
+        else:
+            return json.dumps( dict(data) )
+        
+    @staticmethod
+    def get_user_data(id: int):
+        db_conn = sqlite3.connect(db_path)
+        db_conn.row_factory = sqlite3.Row
+        cursor = db_conn.cursor()
+        
+        cursor.execute("""
+            SELECT email, firstname, lastname, user_type 
+            FROM users
+            WHERE id = ?
+        """, (id,))
+        
+        user_data = cursor.fetchone()
+        cursor.close()
+        db_conn.close()
+        
+        return database.__create_json(user_data)
+    
+    @staticmethod
+    def get_user_id_if_exists(email, password):
+        db_conn = sqlite3.connect(db_path)
+        db_conn.row_factory = sqlite3.Row
+        cursor = db_conn.cursor()
+        
+        cursor.execute("""
+            SELECT id
+            FROM users
+            WHERE email = ? AND pswd = ?
+        """, (email, password))
+        
+        user_id = cursor.fetchone()
+        cursor.close()
+        db_conn.close()
+        
+        return database.__create_json(user_id)
+    
+    
+
+# 
+# 
+#    SELECT (COALESCE(MAX(msg_ordered_number), 0) + 1)
+#    INTO NEW.msg_ordered_number
+#    FROM current_caretaker_conversation_messages
+#    WHERE ccc_id = NEW.ccc_id 
+#
+#        
+        
+        
